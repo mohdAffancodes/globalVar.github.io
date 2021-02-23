63f1fc58a5470fd90f063069e8a9306644dd9120
mohdAffancodes <78096552+mohdAffancodes@users.noreply.github.com>
2021-02-23 15:25:48 +0530

    changed the img

--- global.js
+++ global.js
@@ -1,6 +1,7 @@
 /*
  * Global variables
  * //!'These vars should not be in a function'!
+ * //!'YOU CANNOT USE THESE VARIABLES IN THIS FILE'!
  */
 var string =
    "This the text(which is a string in global.js) displayed from a different js file";
